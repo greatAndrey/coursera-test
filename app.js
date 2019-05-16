0e0c71765f549dea92d8ab747f4117c9f2651cf2
Andrey <greatwwgg@gmail.com>
2019-05-16 22:29:14 +0300

    code refactor, loose coupling

--- app.js
+++ app.js
@@ -3,25 +3,59 @@ var student = {
   type: "student"
 };
 
+var name = "name";
+let totalNameValue = 0;
+
 document.addEventListener('DOMContentLoaded', contentLoaded);
 
 function contentLoaded(event) {
-  document.getElementById('name').addEventListener("keyup", keyUp);
+  document.getElementById(name).addEventListener("keyup", keyUp);
 }
 
 function keyUp(event) {
-  calculateNumericOutput();
+  calculateNumeric(totalNameValue);
 }
 
-function calculateNumericOutput() {
-  student.name = document.getElementById('name').value;
+function calculateNumeric(totalNameValue) {
+  student.name = document.getElementById(name).value;
 
-  var totalNameValue = 0;
   for (var i = 0; i < student.name.length; i++) {
     totalNameValue += student.name.charCodeAt(i);
   }
+      outputStr(totalNameValue);
+}
 
+function outputStr(totalNameValue) {
   // Insert result into page
   var output = "Total Numeric value of person's name is " + totalNameValue;
   document.getElementById('output').innerText = output;
 }
+
+// var student = {
+//   name: "",
+//   type: "student"
+// };
+//
+// document.addEventListener('DOMContentLoaded', contentLoaded);
+//
+// function contentLoaded(event) {
+//   document.getElementById('name').addEventListener("keyup", keyUp);
+// }
+//
+// function keyUp(event) {
+//   calculateNumericOutput();
+// }
+//
+// function calculateNumericOutput() {
+//   student.name = document.getElementById('name').value;
+//
+//   var totalNameValue = 0;
+//   for (var i = 0; i < student.name.length; i++) {
+//     totalNameValue += student.name.charCodeAt(i);
+//      console.log("calc " + totalNameValue);
+//   }
+//
+//   // Insert result into page
+//   var output = "Total Numeric value of person's name is " + totalNameValue;
+//   document.getElementById('output').innerText = output;
+// }
